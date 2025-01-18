1782e83dd8f728996aaf6a2b30029b827bc58f61
Vishwanandini G <gajjalavishwanandini@gmail.com>
2025-01-17 23:59:08 -0800

    pyton-inheritance.py

--- pyton-inheritance.py
+++ pyton-inheritance.py
@@ -18,10 +18,29 @@ class Christmas_Card(Card):
 
     def mssg (self):
         print("Merry Christmas "+self.receiverName+"!!!")
+        
+class Valentine_Card(Card):
+    def __init__(self,receiverName):
+        super().__init__(receiverName)
+
+    def mssg(self):
+        vmssg = f"Happy Valentine's {self.receiverName}!!!"
+        print(vmssg)
+
+class Custom_Card(Card):
+    def __init__(self,receiverName):
+        super().__init__(receiverName)
+        self.cmssg = input("Please enter the custom message: ")
+        
+    def mssg(self):
+        custom_mssg = f"{self.cmssg} {self.receiverName}!!!"
+        print(custom_mssg)
 
 
-c1 = Christmas_Card("Chaitu")
+c1 = Christmas_Card("Sam")
 c1.mssg()
-b1 = Birthday_Card("Siva")
+b1 = Birthday_Card("Sam")
 b1.mssg()
+v1 = valentine_Card("Sam")
+v1.mssg()
 
